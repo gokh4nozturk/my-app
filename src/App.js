977cfc154bc77c3063cedb1f16b4301680463bae
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-04-24 12:21:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HavaDurumu from "./HavaDurumu";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <header className={"App-header"}>
+          <HavaDurumu durum = {"güneşli"}/>
+          <HavaDurumu durum = {"yağmurlu"}/>
+          <HavaDurumu durum = {"bulutlu"}/>
+        </header>
       </header>
     </div>
   );
---
+++ src/HavaDurumu.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HavaDurumu(props) {
+    return <p>bugün hava {props.durum}</p>
+}
+
+export default HavaDurumu;
